b43c7fbc24946489cf7c0341642ea7e00f65b318
Morten Hauge <Morten.hauge97@gmail.com>
2018-12-27 16:56:26 +0100

    Added simple Box2D test and created configuration file

---
+++ configuration/Configuration.h
@@ -0,0 +1,10 @@
+#ifndef NIVEOUS_CONFIGURATION_H
+#define NIVEOUS_CONFIGURATION_H
+
+class Configuration {
+public:
+    static const int SCREENWIDTH = 1920;
+    static const int SCREENHEIGHT = 1080;
+};
+
+#endif //NIVEOUS_CONFIGURATION_H
